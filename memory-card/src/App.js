9fc7553b887ef40da6b498f8218ae598090c19e6
Armando <armando.ortiz.jr.19@outlook.com>
2023-03-03 13:26:01 -0600

    Create basic layout for game

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -1,8 +1,39 @@
 import './App.css';
+import gary from './images/gary.png';
 
 const App = () => {
   return (
-    <div className="App">
+    <div id="whole-game-container">
+      <div id="header">
+        <h2>Memory Card Game</h2>
+        <div id="score-board">
+          <h4>Score: 0</h4>
+          <h4>Best Score: 0</h4>
+        </div>
+      </div>
+      <div id="game-body">
+        <p>Choose a card, but do not choose the same one in the next round</p>
+        <div id="grid-container">
+          <div className="grid-tile">
+            <img src={gary} alt="Gary the snail"/>
+          </div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+          <div className="grid-tile"></div>
+        </div>
+      </div>
     </div>
   );
 }
